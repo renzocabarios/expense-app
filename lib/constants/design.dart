30e927b13f4f99065749a34fbd630422eb4d429e
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-05-12 01:23:57 +0800

    Add Design

---
+++ lib/constants/design.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Design {
+  static const black500 = Color(0xFF1E272E);
+  static const black600 = Color(0xFF1B232A);
+
+  static const white50 = Color(0xFFFDFDFD);
+  static const white300 = Color(0xFFF0F0F1);
+
+  static const red600 = Color(0xFFD31701);
+
+  static const blue600 = Color(0xFF3B79DE);
+}
