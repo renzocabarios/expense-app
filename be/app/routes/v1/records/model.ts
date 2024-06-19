9bfb5041ee248b43e98718f294e8943dbce57db6
renzocabarios <renzo.cabarios@gmail.com>
2024-06-19 18:26:17 +0800

    Update records model

--- be/app/routes/v1/records/model.ts
+++ be/app/routes/v1/records/model.ts
@@ -5,19 +5,26 @@ const option = {
   timestamps: true,
 };
 
-const schema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  value: {
-    type: Number,
-    default: 0,
-  },
-  deleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+  option
+);
 
 export default model(RESOURCE.RECORDS, schema);
--- be/tests/records-controller.test.ts
+++ be/tests/records-controller.test.ts
@@ -4,13 +4,27 @@ import { getRandomNumber } from "./helpers";
 
 describe("records controller", () => {
   it("should create a record", async () => {
+    let user: any = {
+      email: "sample@gmail.com",
+      password: "password",
+    };
+
+    user = (
+      await request(app)
+        .post("/api/v1/users")
+        .send({ ...user })
+    ).body.data[0];
+
     await request(app)
       .post("/api/v1/records")
       .send({
         name: "Test",
         value: getRandomNumber(1, 10000),
+        user: user._id,
       });
+
     const res = await request(app).get("/api/v1/records");
+
     expect(res.body.data.length).toBeGreaterThan(0);
   });
 });
